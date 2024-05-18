a9ccdb5bc1db01d5ef1a099f98512ba3727ea679
uzairshakeel123 <116012853+uzairshakeel123@users.noreply.github.com>
2024-05-18 14:48:43 +0500

    Add files via upload

---
+++ Allquestion.js
@@ -0,0 +1,13 @@
+// Question Number 02
+// let person_Name = "uzair kaim khani";
+// console.log ("Assalam-o-Alikum, ",person_Name," learn the Python today!");
+// Question Number 03
+var person_Name = "uzair kk";
+console.log(person_Name.toLowerCase());
+console.log(person_Name.toUpperCase());
+console.log(person_Name.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
+// Question Number 04
+// var quote = "Hello Kamran Khan Tessori";
+// console.log("Governor Sindh, ", "\"", quote, "\"");
+// console.log("Hello,\"A man is known by the company he keeps\"");
+//  Question Number 05
